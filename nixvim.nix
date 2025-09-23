12424bfa1d2c23976e8222797687be766b506aff
nao710 <117145517+nao710@users.noreply.github.com>
2025-09-23 21:37:57 +0900

    Update nixvim.nix

--- nixvim.nix
+++ nixvim.nix
@@ -2,6 +2,36 @@
   programs.nixvim = {
     enable = true;
     colorschemes.catppuccin.enable = true;
-    plugins.lualine.enable = true;
+    { pkgs, ... }:
+
+{
+    plugins = {
+      lsp = {
+        enable = true;
+        servers.nil_ls.enable = true;
+      };
+
+      lsp-format = {
+        enable = true;
+      };
+    };
+    extraConfigLua = ''
+      require("lspconfig").nil_ls.setup {
+        settings = {
+          ["nil"] = {
+            formatting = {
+              command = { "nixfmt" },
+            },
+          },
+        },
+      }
+    '';
+    
+    extraPackages = with pkgs; [
+      nixfmt-rfc-style
+    ];
+}
+
+
   };
 }
