21615d3e8137d05249895424c6ba80ddfb87088e
nao710 <117145517+nao710@users.noreply.github.com>
2025-09-23 21:38:54 +0900

    Create git.nix

---
+++ git.nix
@@ -0,0 +1,15 @@
+{
+programs.git = {
+  enable = true;
+  userName = "nao710";
+  userEmail = "naototanaoto@outlook.com";
+  extraConfig = {
+    core = {
+      editor = "neovim";
+    };
+    init = {
+      defaultBranch = "main";
+    };
+  };
+};
+}
