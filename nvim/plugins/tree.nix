b65fb1f681718a497a2c0846b08971c76baabeee
nao710 <naototanaoto@outlook.jp>
2025-09-28 11:10:42 +0900

    update

--- nvim/plugins/tree.nix
+++ nvim/plugins/tree.nix
@@ -1,37 +1,38 @@
 {
-  lir = {
+  plugins.lir = {
     enable = true;
-    devicons = {
-      enable = false;
+    settings = {
+      devicons = {
+        enable = false;
+      };
+      hide_cursor = true;
+      mappings = {
+        "j" = {
+          __raw = "require('lir').actions.up";
+        };
+        "<CR>" = {
+          __raw = "require('lir').actions.edit";
+        };
+        "<ESC>" = {
+          __raw = "require('lir').actions.quit";
+        };
+        "o" = {
+          __raw = "require('lir').actions.cd";
+        };
+        "m" = {
+          __raw = "require('lir').actions.mkdir";
+        };
+        "n" = {
+          __raw = "require('lir').actions.newfile";
+        };
+        "R" = {
+          __raw = "require('lir').actions.rename";
+        };
+        "D" = {
+          __raw = "require('lir').actions.delete";
+        };
+      };
+      show_hidden_files = true;
     };
-    hide_cursor = true;
-    mappings = {
-      "j" = {
-        __raw = "require('lir').actions.up";
-      };
-      "<CR>" = {
-        __raw = "require('lir').actions.edit";
-      };
-      "<ESC>" = {
-        __raw = "require('lir').actions.quit";
-      };
-      "o" = {
-        __raw = "require('lir').actions.cd";
-      }; 
-      "m" = {
-        __raw = "require('lir').actions.mkdir";
-      };
-      "n" = {
-        __raw = "require('lir').actions.newfile";
-      };
-      "R" = {
-        __raw = "require('lir').actions.rename";
-      };
-      "D" = {
-        __raw = "require('lir').actions.delete";
-      };
-    };
-    show_hidden_files = true;
   };
-
 }
