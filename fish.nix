f18bc7c1a0e27f3435ec789b6b06579fad9ba8f3
nao710 <naototanaoto@outlook.jp>
2025-09-28 14:36:07 +0900

    add fish

---
+++ fish.nix
@@ -0,0 +1,68 @@
+{ pkgs, ... }:
+{
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      g = "git";
+      cp = "cp -i";
+      rm = "rm -i";
+      gl = "git pull";
+      gps = "git push";
+      gcm = "git commit -m";
+      gs = "git status";
+      ga = "git add";
+      gcl = "gh repo clone";
+      gc = "git commit";
+    };
+    functions.cd = {
+      body = ''
+        if test (count $argv) -eq 0
+            builtin cd ~
+            set -l cd_status $status
+        else if test (count $argv) -gt 1
+            printf "%s\n" (_ "Too many args for cd command")
+            return 1
+        else
+            if test "$argv" = "-"
+                if test "$__fish_cd_direction" = "next"
+                    nextd
+                else
+                    prevd
+                end
+                return $status
+            end
+            set -l previous $PWD
+            builtin cd $argv
+            set -l cd_status $status
+        end
+
+        if test $cd_status -eq 0 -a "$PWD" != "$previous"
+            set -q dirprev[$MAX_DIR_HIST]
+            and set -e dirprev[1]
+            set -g dirprev $dirprev $previous
+            set -e dirnext
+            set -g __fish_cd_direction prev
+        end
+
+        if test $cd_status -ne 0
+            return 1
+        end
+
+        ls
+        return $status
+      '';
+    };
+  };
+
+  programs.bash = {
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+}
