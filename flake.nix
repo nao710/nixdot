c8fdd7f60145f05c2b49bc5fdf44dd6546b79c1b
nao710 <117145517+nao710@users.noreply.github.com>
2026-02-14 16:20:58 +0900

    Fix formatting in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nixos config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11"; # github: を明記するのが無難
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
