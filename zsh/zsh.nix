f81c7cc5996d0723f756a158d6ad037d0186ec2e
nao710 <naototanaoto@outlook.com>
2026-02-22 21:15:35 +0900

    update

--- zsh/zsh.nix
+++ zsh/zsh.nix
@@ -3,7 +3,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    autosuggestions.enable = true;
+    # autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     histSize = 10000;
     promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
@@ -23,6 +23,7 @@
       ga = "git add";
       gcl = "gh repo clone";
       gc = "git commit";
+      gd = "git diff";
     };
     ohMyZsh = {
       enable = true;
