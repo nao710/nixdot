4287eaca716406299f22a136e38e3924f10f132f
nao710 <naototanaoto@outlook.com>
2026-02-22 19:10:58 +0900

    update

--- git.nix
+++ git.nix
@@ -1,15 +1,13 @@
 {
   programs.git = {
     enable = true;
-    settings = {
-      user.email = "naototanaoto@outlook.com";
-      user.mame = "nao710";
-      core = {
-        editor = "neovim";
-      };
-      init = {
-        defaultBranch = "main";
-      };
+    userName = "nao710";
+    userEmail = "naototanaoto@outlook.com";
+
+    extraConfig = {
+      core.editor = "neovim";
+      init.defaultBranch = "main";
+      credential."https://github.com".helper = "!gh auth git-credential";
     };
   };
 }
